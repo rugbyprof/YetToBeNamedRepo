976054d7590313c16a861b38d2fab69dfeedd931
rugbyprof <terry.griffin@msutexas.edu>
2022-03-11 14:58:19 -0600

    added folders

---
+++ code/main.cpp
@@ -0,0 +1,20 @@
+#include "bst.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  Bst tree;
+  tree.Insert(17);
+  tree.Insert(18);
+  tree.Insert(33);
+  tree.Insert(41);
+  tree.Insert(91);
+  tree.Insert(106);
+
+  tree.Print();
+  int k = 17;
+  cout << "Looking for " << k << ":" << tree.Find(k) << endl;
+
+  cout << "Height: " << tree.Height() << endl;
+}
